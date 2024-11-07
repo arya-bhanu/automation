5b2ba70c52f223a570e27265c49f7e83e5e6bb7c
arya-bhanu <aryacreative.dev@gmail.com>
2024-11-07 16:35:22 +0700

    extracted

---
+++ reports-lighthouse/extract.js
@@ -0,0 +1,49 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import xlsx from 'xlsx';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function extractData(filePath) {
+	const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+	const extractedData = {
+		firstContentfulPaint: data.audits['first-contentful-paint'].numericValue,
+		totalBlockingTime: data.audits['total-blocking-time'].numericValue,
+		speedIndex: data.audits['speed-index'].numericValue,
+		largestContentfulPaint:
+			data.audits['largest-contentful-paint'].numericValue,
+		cumulativeLayoutShift: data.audits['cumulative-layout-shift'].numericValue,
+		performanceScore: data.categories.performance.score,
+	};
+
+	return extractedData;
+}
+
+function processDirectory(directoryPath, name) {
+	let dataJson = [];
+	const workbook = xlsx.utils.book_new();
+	fs.readdirSync(directoryPath).forEach((file) => {
+		const filePath = path.join(directoryPath, file);
+		const stats = fs.statSync(filePath);
+
+		if (stats.isDirectory()) {
+			processDirectory(filePath);
+		} else if (path.extname(filePath) === '.json') {
+			const extractedData = extractData(filePath);
+			dataJson.push(extractedData);
+		}
+	});
+	const worksheet = xlsx.utils.json_to_sheet(dataJson);
+	xlsx.utils.book_append_sheet(workbook, worksheet, `Sheet${name}`);
+	xlsx.writeFile(workbook, `output_${name}.xlsx`);
+}
+
+const reportsDir = path.join(__dirname, 'client-side', 'home-portal');
+
+console.log(reportsDir);
+
+processDirectory(reportsDir, 'client-side-home-portal');
